0c1476b62760a9b198a4de1546a59fef8e31c382
Fabian Flores <103683356+famerca@users.noreply.github.com>
2025-06-02 08:28:33 -0400

    Add files via upload

---
+++ Presentacion/prueba.py
@@ -0,0 +1,12 @@
+from time import sleep
+def foo():
+    sleep(0.1)
+
+
+for i in range(0,50):
+    print("numero",i)
+    foo()
+    if i > 20:
+        print(p)
+
+
---
+++ Presentacion/prueba2.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+def foo():
+    sleep(0.1)
+
+for i in range(0,50):
+    print("numero", i)
+    foo()
+    if(i > 20):
+        print("numero" + i)
